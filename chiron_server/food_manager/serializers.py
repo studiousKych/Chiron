9a04ea499394a6a48be33830c8af1d0a48e76eea
Chirag Karia <chirag.karia@uoit.net>
2017-12-14 21:29:08 -0500

    Remove unnecessary user field for view

--- chiron_server/food_manager/serializers.py
+++ chiron_server/food_manager/serializers.py
@@ -4,9 +4,9 @@ from .models import DietProfile
 
 class DietProfileSerializer(serializers.ModelSerializer):
 
-    user = serializers.ReadOnlyField(source='user.email')
+    # user = serializers.ReadOnlyField(source='user.email')
 
     class Meta:
         model = DietProfile
-        fields = ('user', 'name', 'staged_meals')
+        fields = ('name', 'staged_meals')
         depth=3
