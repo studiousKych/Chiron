5d6089adf7e9a84a1437c5898196a2a0aa13a2a8
Chirag Karia <chirag.karia@uoit.net>
2017-12-15 03:25:06 -0500

    Add routes

--- chiron_server/food_manager/urls.py
+++ chiron_server/food_manager/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.authtoken.views import obtain_auth_token
 from .views import DietProfileView
 
 urlpatterns = {
     url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^diets/$', DietProfileView.as_view(), name='viewdiets')
+    url(r'^diets/$', DietProfileView.as_view(), name='viewdiets'),
+    url(r'^get-token/', obtain_auth_token)
     # url(r'^stagedmeals/$', StagedMealView.as_view(), name='create'),
     # url(r'^setcourses/$', SetCourseView.as_view(), name='viewsetcourse')
 }
