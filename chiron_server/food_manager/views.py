a8313dbd769bcdabeca30cf06a17e2441421e9df
Chirag Karia <chirag.karia@uoit.net>
2017-12-15 17:10:13 -0500

    start adding authentication to server

--- chiron_server/food_manager/views.py
+++ chiron_server/food_manager/views.py
@@ -10,9 +10,9 @@ class DietProfileView(generics.RetrieveAPIView):
 
 
     serializer_class = DietProfileSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticated, IsUser)
 
     def get_object(self):
         user = self.request.user
 
-        return DietProfile.objects.first()
+        return DietProfile.objects.get(user=user)
