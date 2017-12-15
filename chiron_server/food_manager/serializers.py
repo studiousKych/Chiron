42cb2676652e676d93af577fbccf95e3fe5bee75
Chirag Karia <chirag.karia@uoit.net>
2017-12-14 20:07:59 -0500

    Clean up serializers file

--- chiron_server/food_manager/serializers.py
+++ chiron_server/food_manager/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
-from .models import StagedMeal
+from .models import DietProfile
 
-class StagedMealSerializer(serializers.ModelSerializer):
+
+class DietProfileSerializer(serializers.ModelSerializer):
+
+    user = serializers.ReadOnlyField(source='user.email')
 
     class Meta:
-        model = StagedMeal
-        read_onlyfields = ('diet_profile', 'day', 'num', 'meal_type', 'items')
+        model = DietProfile
+        fields = ('user', 'name', 'staged_meals')
+        depth=3
