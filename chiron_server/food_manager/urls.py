3ae30f22741a031b90600fb33218e26ac4f1a8ce
Chirag Karia <chirag.karia@uoit.net>
2017-12-14 20:10:36 -0500

    Clean up urls file

--- chiron_server/food_manager/urls.py
+++ chiron_server/food_manager/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import CreateView
+from .views import DietProfileView
 
 urlpatterns = {
-    url(r'^stagedmeals/$', CreateView.as_view(), name='create')
+    url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^diets/$', DietProfileView.as_view(), name='viewdiets')
+    # url(r'^stagedmeals/$', StagedMealView.as_view(), name='create'),
+    # url(r'^setcourses/$', SetCourseView.as_view(), name='viewsetcourse')
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
