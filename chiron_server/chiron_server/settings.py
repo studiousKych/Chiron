0a74d39cde41472159b35378e5ef7f9d335ab2a9
Chirag Karia <chirag.karia@uoit.net>
2017-12-15 12:40:00 -0500

    Add support for xml result

--- chiron_server/chiron_server/settings.py
+++ chiron_server/chiron_server/settings.py
@@ -22,6 +22,11 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework_xml.renderers.XMLRenderer'
     )
 }
 
--- chiron_server/food_manager/serializers.py
+++ chiron_server/food_manager/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
+from rest_framework.renderers import JSONRenderer
+from rest_framework_xml.renderers import XMLRenderer
+from rest_framework.views import APIView
 from .models import DietProfile
 
 
 class DietProfileSerializer(serializers.ModelSerializer):
-
-    # user = serializers.ReadOnlyField(source='user.email')
-
     class Meta:
         model = DietProfile
         fields = ('name', 'data_type', 'staged_meals')
